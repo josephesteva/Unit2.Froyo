dcbf69b2a564f00107f4305fe91ce1228bdaf465
josephesteva <joe.esteva@gmail.com>
2023-10-31 16:04:18 -0400

    Update variable names

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //get a list of flavors from the user
 const flavors = prompt("What Froyo flavors would you like? ");
 
-// console.log(flavors)
-
 const flavorCount = (flavorList) => {
 	//separate list into an array of values (use split with comma)
 	const flavorArray = flavorList.split(`,`);
@@ -10,22 +8,14 @@ const flavorCount = (flavorList) => {
 	const flavorCount = {};
 	//iterate through each item in the array
 	for (i = 0; i < flavorArray.length; i++) {
-		flavorCount[flavorArray[i]] = (flavorCount[flavorArray[i]] + 1) || 1;
-
-
-
-
-
-
-
-		// //if flavor does not exist in object, create a new key and set value to 1
-		// if (flavorCount[flavorArray[i]] == null) {
-		// 	flavorCount[flavorArray[i]] = 1;
-		// } 
-		// //if flavor does exist, increment count by 1
-		// else {
-		// 	flavorCount[flavorArray[i]]++;
-		// }
+		//if flavor does not exist in object, create a new key and set value to 1
+		if (flavorCount[flavorArray[i]] == null) {
+			flavorCount[flavorArray[i]] = 1;
+		} 
+		//if flavor does exist, increment count by 1
+		else {
+			flavorCount[flavorArray[i]]++;
+		}
 	}
 	return flavorCount;
 	}
